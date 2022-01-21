da4e857779a38b0b51085b543fd868795059e5eb
makskit0 <97789002+makskit0@users.noreply.github.com>
2022-01-21 11:38:47 +0300

    Add files via upload

--- Quiz.py
+++ Quiz.py
@@ -6,34 +6,32 @@ class Quiz():
         self.score = score
 
     def ask_question(self):
-        print(question_1)
-        print(question_2)
-        print(question_3)
-        print(question_4)
+        print(question)
 
     def check_the_answer(self):
-        if user_answer == question_and_answer["Начало Второй Мировой Войны ?"]:
+        if user_answer == question_and_answer["question_1"]:
             print("Верный ответ!")
             self.score += 1
-        else:
-            print(question_and_answer)
 
+    def get_score(self ):
+        print(self.score)
+
+question = "1. Начало второй мировой войны ? "
 
 question_and_answer = {
-    "Начало Второй Мировой Войны ?" : 1939,
-    "два плюс два ?" : 4 ,
-    "корень из 25 ?" : 5,
-    "английский перевод слова вопрос ?" : "question"
+    "question_1" : 1939,
+    "question_2" : 4 ,
+    "question_3" : 5,
+    "question_4" : "question"
     
 }
-score = 0
-question = Quiz()
-print(question.ask_question())
-user_answer = input("Введите ваш ответ:")
-check_answer = Quiz()
-check_answer.check_the_answer()
-question_1 = ("Начало Второй Мировой Войны ?" )
-question_2 = ("два плюс два ?" )
-question_3 = ("корень из 25 ? " )
-question_4 = ("английский перевод слова вопрос ? " )
 
+
+question_1 = Quiz("Начало Второй Мировой Войны ?" , 1939 , 0 )
+question_2 = Quiz("два плюс два ?", 4 , 0 )
+question_3 = Quiz("корень из 25 ? ", 5, 0 )
+question_4 = Quiz("английский перевод слова вопрос ? " , "question" , 0)
+question_1.ask_question()
+user_answer = input("Введите ваш ответ:")
+question_1.check_the_answer()
+question_1.get_score()
